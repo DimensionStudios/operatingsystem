27370ed70748c5ae0ffbba5aaa4a5c831d8dabae
DimensionStudios <piggyandbobo@icloud.com>
2025-07-26 08:54:18 -0700

    currently being used to learn c++

--- main.cpp
+++ main.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
+#include <string>
 #include <print>
 
-int main(){
-    std::println("Hello");
+int main() {
+    std::string name;
+    int age;
+
+    std::println("Name: ");
+    std::getline(std::cin, name);
+
+    std::println("Age: ");
+    std::cin >> age;
+
+    std::cout << "Your name is " << name << ". And you are " << age << " years old\n";
 }
